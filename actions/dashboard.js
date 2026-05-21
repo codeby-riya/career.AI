94c8c577f39be933c95e5713795ae03eee039441
codeby-riya <codebyriya@gmail.com>
2026-05-22 03:55:20 +0530

    change model

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -5,7 +5,7 @@ import { auth } from "@clerk/nextjs/server";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2.5-pro" });
+const model = genAI.getGenerativeModel({ model: "gemini-3.5-flash" });
 
 export const generateAIInsights = async (industry) => {
   const prompt = `
