01e5f04a91408661d0ce041a2df2ca48cc4709e1
codeby-riya <codebyriya@gmail.com>
2026-05-22 01:15:33 +0530

    changes

--- data/testimonial.js
+++ data/testimonial.js
@@ -10,7 +10,7 @@ export const testimonial = [
   {
     quote:
       "The industry insights helped me pivot my career successfully. The salary data was spot-on!",
-    author: "David",
+    author: "Harshit",
     image: "https://randomuser.me/api/portraits/men/75.jpg",
     role: "Product Manager",
     company: "StartUp Inc.",
@@ -18,7 +18,7 @@ export const testimonial = [
   {
     quote:
       "My resume's ATS score improved significantly. Got more interviews in two weeks than in six months!",
-    author: "Elena",
+    author: "Kriti",
     image: "https://randomuser.me/api/portraits/women/74.jpg",
     role: "Marketing Director",
     company: "Global Corp",
